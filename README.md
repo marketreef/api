d0febe49195a54a4288205b5dcc0768e86ad81c8
Fred Cox (Neptune) <fred@smartbotsmedia.com>
2013-07-11 17:53:37 -0300

    Std Lib + Docs

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 Market Reef API Module
 
-more documentation coming soon...
+Sample code:
+
+import marketreef
+client = MarketReef("USERNAME", "PASSWORD")
+
+
+# Latest performance on Homebuilders
+name = 'portfolio_performance'
+data = {'p':'XHB'}
+report = client.report(name, data)
+
+print report['title']
+print report['text']
+
+
+# Latest performance on the Tech, Financials and Health Care Sectors
+name = 'portfolio_performance'
+data = [{'p':'XLK'},
+        {'p':'XLF'},
+        {'p':'XLV'}]
+report = client.report(name, data)
+
+for report in reports:
+    print report['title']	
+    print report['text']
+
+# By personal portfolio YTD
+name = 'portfolio_performance'
+data = {'p':'AAPL 20%, KO 20%, GOOG 15%, BRK-B 15%, XHB 10%, CMCSA 5%, FB 5%, ZNGA 5%, LNKD 5%', 's':"2012-12-31"}
+report = client.report(name, data)
+
+for report in reports:
+    print report['title']	
+    print report['text']
--- marketreef.py
+++ marketreef.py
@@ -1,34 +1,18 @@
-import requests
-import os
+import urllib
+import urllib2
+import json
+import cookielib
 import threading
 import Queue
 
-class ReportThread(threading.Thread):
-    def __init__(self, report_url, login_cookies, 
-                 data_queue, report_queue):
-        threading.Thread.__init__(self)
-        self.report = report_url
-        self.cookies = login_cookies
-        self.data = data_queue
-        self.output = report_queue
-    def run(self):
-        while True:
-            data = self.data.get()
-            try:
-                r = requests.post(self.report, cookies=self.cookies, 
-                                  data=data, timeout = 5)
-
-                self.output.put(r)
-            except:
-                print "ERROR! %s" % (data)
-            self.data.task_done()
-
 class MarketReef(object):
     '''
+    Market Reef API client object.
     '''
-    def __init__(self, username = None, password = None, 
+    def __init__(self, username, password, 
                  url=''):
         '''
+        Construct client by supplying your username and password.
         '''
         if not url:
             self.url = 'http://api.marketreef.co/'
@@ -36,22 +20,51 @@ class MarketReef(object):
             self.url = url
         self.login_ext = 'login'
         if username and password:
-            self.login(username, password)
+            try:
+                self._login(username, password)
+            except:
+                raise Exception("Failed to login")
 
-    def login(self, username, password):
-        '''Login method.'''
-        client = requests.session()
-        client.get(self.url)
-        data = dict(username=username, password=password)
-        r = client.post(self.url + self.login_ext, data=data)
-        self.cookies = dict(sessionid=r.cookies['sessionid'])
+    def _login(self, username, password):
+        cj = cookielib.CookieJar()
+        cookiehandler = urllib2.HTTPCookieProcessor(cj)
+        opener = urllib2.build_opener(cookiehandler)
+        
+        data = "username=%s&password=%s" % (username,
+                                            password)
 
+        r = opener.open(self.url + self.login_ext,
+                        data, 5).read()
+
+        assert r == "Logged In"
+        
+        urllib2.install_opener(opener)
+            
     def report(self, name, data):
-        '''Reporting method'''
+        '''
+        The 'report' method takes two arguments:
+        'name' refers to the name of the report. "portfolio_performace" is
+        the only supported report at this point.
+
+        The 'data' argument takes in a dictionary with the information to
+        build the report from. See http://www.marketreef.co/api/docs for
+        information on the arguments accepted. 
+
+        The 'data' argument also accepts a list of valid data objects. We
+        recommend using this method instead of calling the 'report' method
+        multiple times to generate reports individually it provides
+        significantly faster results. Billing is not affected either way.
+
+        The method returns a dictionary or list of dictionaries containing
+        the rendered reports. See documentation for specific fields.
+        '''
         report = self.url + name
         if type(data) != list:
             # Single Report Request
-            r = requests.post(report, cookies=self.cookies, data=data)
+            if type(data) == dict:
+                data = urllib.urlencode(data)
+            r = urllib2.urlopen(report, data)
+            r = json.load(r)
             return r
         else:
             # Multiple Reports Requested
@@ -61,8 +74,7 @@ class MarketReef(object):
             threads = min(len(data), 5)
 
             for x in range(threads):
-                t = ReportThread(report, self.cookies, 
-                                 data_queue, output_queue)
+                t = _ReportThread(report, data_queue, output_queue)
                 t.setDaemon(True)
                 t.start()
             
@@ -78,3 +90,62 @@ class MarketReef(object):
 
             return output
         
+class _ReportThread(threading.Thread):
+    '''
+    INTERNAL USE
+    Thread for a single query.
+    '''
+    def __init__(self, report_url, data_queue, report_queue):
+        '''
+        Constructor sets the authentication url, cookies, input and 
+        output queues.
+        '''
+        threading.Thread.__init__(self)
+        self.report = report_url
+        self.data = data_queue
+        self.output = report_queue
+
+    def run(self):
+        '''
+        Actual process, grabs jobs from input queue, renders report, then
+        puts to the output queue.
+        '''
+        while True:
+            data = self.data.get()
+            try:
+                if type(data) == dict:
+                    data = urllib.urlencode(data)
+                r = urllib2.urlopen(self.report, data, 5)
+                r = json.load(r)
+
+                self.output.put(r)
+            except:
+                print "ERROR! %s" % (data)
+            self.data.task_done()
+
+if __name__ == "__main__":
+    import os
+    client = MarketReef(os.environ['API_USER'], os.environ['API_PWD'])
+    data = [{'p':'XLK 20.79%, XLF 16.60%, XLV 12.63%, XLY 12.30%, XLE 10.56%, XLP 10.49%, XLI 10.10%, XLB 3.27%, XLU 3.25%'},
+            {'p':'XLK'},
+            {'p':'XLF'},
+            {'p':'XLV'},
+            {'p':'XLY'},
+            {'p':'XLE'},
+            {'p':'XLP'},
+            {'p':'XLI'},
+            {'p':'XLB'},
+            {'p':'XLU'},
+        ]
+    reports = client.report('portfolio_performance', data)
+    
+    for r in reports:
+        if r['meta']['status'] == '200':
+            print r['title']
+            print
+            print r['text']
+            print
+            print r['tweet']
+            print
+            print
+                
